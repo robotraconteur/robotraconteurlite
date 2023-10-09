2acf517e852331191497e6d937d1833e97e39f82
John Wason <wason@wasontech.com>
2023-10-09 00:02:09 -0400

    Handle EINTR poll error correctly (#3)

--- src/poll_posix.c
+++ src/poll_posix.c
@@ -4,11 +4,16 @@
 #include <poll.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <errno.h>
 
 
 int robotraconteurlite_poll(struct robotraconteurlite_pollfd* fds, int nfds, int timeout)
 {
     int ret;
-    ret = poll((struct pollfd*)fds, (int)nfds, (int)timeout); 
+    ret = poll((struct pollfd*)fds, (int)nfds, (int)timeout);
+    if (ret < 0 && errno == EINTR)
+    {
+        return 0;
+    }
     return ret;
 }
