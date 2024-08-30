66e2b56dbbb881ea86495cacaa8b0b67b99296a0
John Wason <wason@wasontech.com>
2024-08-30 09:22:28 -0400

    Use ctest to launch tiny tests

--- test/tiny_test/tiny_client_ci_test.py
+++ test/tiny_test/tiny_client_ci_test.py
@@ -2,11 +2,18 @@ import subprocess
 import sys
 from pathlib import Path
 import time
-import os
+import argparse
+
+parser = argparse.ArgumentParser(description='Run tiny client CI test')
+parser.add_argument('--build-dir', type=str, help='Build directory', required=False, default=None)
+args = parser.parse_args()
 
 script_dir = Path(__file__).parent
-repo_dir = script_dir.parent.parent
-examples_build_dir = Path(os.environ.get("RRLITE_EXAMPLES_BUILD_DIR", (repo_dir / "build" / "examples").absolute()))
+repo_dir = script_dir.parent.parent.absolute()
+examples_build_dir = args.build_dir
+if examples_build_dir is None:
+    examples_build_dir = (repo_dir / "build" / "examples").absolute()
+examples_build_dir = Path(examples_build_dir)
 print(examples_build_dir)
 
 py_dir = (repo_dir / "examples" / "tiny_client").absolute()
