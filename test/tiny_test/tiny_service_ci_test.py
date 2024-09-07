5cd72b5d82e0870ece2e3f173519a3d749a19351
John Wason <wason@wasontech.com>
2024-09-06 23:10:43 -0400

    Kill tiny_service on error

--- test/tiny_test/tiny_service_ci_test.py
+++ test/tiny_test/tiny_service_ci_test.py
@@ -19,16 +19,18 @@ print(examples_build_dir)
 
 py_dir = (repo_dir / "examples" / "tiny_service").absolute()
 
-service = subprocess.Popen([examples_build_dir / "robotraconteurlite_tiny_service"],
-                           cwd=examples_build_dir, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-time.sleep(2)
+try:
 
+    service = subprocess.Popen([examples_build_dir / "robotraconteurlite_tiny_service"],
+                               cwd=examples_build_dir, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    time.sleep(2)
 
-subprocess.check_call([sys.executable, py_dir / "tiny_service_client.py"], cwd=py_dir)
+    subprocess.check_call([sys.executable, py_dir / "tiny_service_client.py"], cwd=py_dir)
+finally:
 
-time.sleep(0.5)
-service.send_signal(signal.SIGINT)
-service.wait()
+    time.sleep(0.5)
+    service.send_signal(signal.SIGINT)
+    service.wait()
 
 # Check return code
 if service.returncode != 0:
